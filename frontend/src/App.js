0b763f3c9a7d40a7dc8329924bff0e6de3784b41
Nick <Npgb18@gmail.com>
2023-11-09 13:16:26 +0000

    books commented out for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Books from './pages/Books';
+// import Books from './pages/Books';
 import Home from './pages/Home';
 import { NavbarWithSearch } from './components/NavbarWithSearch';
 
@@ -16,10 +16,10 @@ function App() {
               path="/" 
               element={<Home />}
             />
-            <Route 
+            {/* <Route 
               path="/books" 
               element={<Books />}
-            />
+            /> */}
 
           </Routes>
         </div>
