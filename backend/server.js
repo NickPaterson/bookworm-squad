96a8096b427c13c87338ae2dfdbd3c104f6f5749
Nick <Npgb18@gmail.com>
2023-12-01 17:12:23 +0000

    bug fixes

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,8 @@ app.get("*", (req, res) => {
 // }
 
 // Connect to MongoDB
-// mongoose.connect(process.env.MONGODB_URI)
-mongoose.connect("mongodb://mongodb/")
+mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect("mongodb://mongodb/")
     .then(() => {
         // Listen to port
         app.listen(port, () => {
