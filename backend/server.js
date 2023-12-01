2127a53a604350c7ac0fbf6bfef34398950bfce7
Nick <Npgb18@gmail.com>
2023-12-01 15:45:10 +0000

    .

--- backend/server.js
+++ backend/server.js
@@ -32,13 +32,14 @@ app.get("*", (req, res) => {
 });
 
 // Exit the application if MongoDB URI is not defined
-if (!process.env.MONGODB_URI) {
-    console.error('MONGODB_URI environment variable is not defined.');
-    process.exit(1); 
-}
+// if (!process.env.MONGODB_URI) {
+//     console.error('MONGODB_URI environment variable is not defined.');
+//     process.exit(1); 
+// }
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+// mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb://mongodb/")
     .then(() => {
         // Listen to port
         app.listen(port, () => {
