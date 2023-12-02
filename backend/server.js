b4fabaa25a24e6d7d2c98863bfc5b9d9710b81ba
Nick <Npgb18@gmail.com>
2023-12-02 17:53:49 +0000

    package.json homepage changed, favicon updated

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,7 @@ app.use('/api/books', booksRoutes);
 // production
 app.use(express.static('frontend/build'));
 app.get("*", (req, res) => {
-    //res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-
 });
 
 // Exit the application if MongoDB URI is not defined
