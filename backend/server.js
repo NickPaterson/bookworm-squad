938e4fb42611da002f092b6864aca5cc807866e0
Nick <Npgb18@gmail.com>
2023-12-02 19:00:40 +0000

    react build

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,9 @@ app.use((req, res, next) => {
 app.use('/api/books', booksRoutes);
 
 // production
-app.use(express.static(path.join(__dirname, 'frontend')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'frontend', 'public', 'index.html'));
+app.use(express.static('frontend/build'));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 // Exit the application if MongoDB URI is not defined
